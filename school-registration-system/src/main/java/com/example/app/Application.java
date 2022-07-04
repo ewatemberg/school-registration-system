add3cd393419631a905b495fbbf249be894ad41a
a310759 <ewatemberg@santandertecnologia.com.ar>
2022-07-04 16:08:58 -0300

    feat(ConfigDependencies): add the following dependecies: groovy, spock, open-api, flyway and customize the README.md documentation

--- school-registration-system/src/main/java/com/example/app/Application.java
+++ school-registration-system/src/main/java/com/example/app/Application.java
@@ -1,13 +1,13 @@
-package com.example.schoolregistrationsystem;
+package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolRegistrationSystemApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SchoolRegistrationSystemApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }

f264b888c131bd7e2c54b698bb510f1adc822c4c
Emilio Watemberg <emilio.watemberg@gmail.com>
2022-07-04 20:02:28 -0300

    feat(BuildDomain): reorganize files and folder.

--- school-registration-system/src/main/resources/db/migration/V1.3__COURSE_create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `course`
-(
-    `id`               bigint  NOT NULL AUTO_INCREMENT,
-    `name`             varchar(100) DEFAULT NOT NULL,
-    `maximum_capacity` TINYINT NOT NULL,
-    PRIMARY KEY (`id`)
-);
